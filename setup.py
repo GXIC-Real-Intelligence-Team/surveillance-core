2c0c413d6027a57953ed16e4a963e7f3ff8adca6
ruohan.chen <crhan123@gmail.com>
2017-08-26 16:06:19 +0800

    setup module structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="gxic_rit",
+    version="0.0.1",
+    author_email="crhan123@gmail.com",
+    author="ruohan.chen",
+    packages=find_packages(),
+)
