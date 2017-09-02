9b1eedc5f78c513b9680662e5ca732976d663568
jinshuai.js <kingskywalker.jin@gmail.com>
2017-09-02 19:37:56 +0800

    add face handling

--- gxic_rit/bee/stream.py
+++ gxic_rit/bee/stream.py
@@ -1,12 +1,91 @@
 import cv2
 import subprocess
 import argparse
+import json
+import requests
+from gxic_rit.konan import faceapi
+from gxic_rit.konan import image
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--debug", help="debug mode, display frame", action="store_true")
 parser.add_argument("-i", "--input", help="input rtmp stream", type=str, required=True)
 parser.add_argument("-o", "--output", help="input rtmp stream", type=str, required=True)
 parser.add_argument("-s", "--size", help="output video size, like 640x480", type=str, default="640x480")
+args = parser.parse_args()
+
+face_seq = 0
+
+def request(ip, port, path, data=""):
+    uri = "http://%s:%s/%s" % (ip, port, path)
+    req = requests.post(uri, data=data)
+    ret = json.loads(req.content)
+
+    if args.debug:
+        print(ret)
+
+    return ret
+
+
+def predict_face(peoples):
+    data = []
+
+    for people in peoples:
+        item = {
+            "seq": people['seq'],
+            "eigen": people['eigen']
+        }
+        data.append(item)
+
+    ret = request('127.0.0.1', 5000, "/predict", data=json.dumps(data))
+
+    for item in ret:
+        for people in peoples:
+            if item['seq'] != people['seq']:
+                continue
+
+            if item['people']['confidence'] < 0.7:
+                people['id'] = -1
+                people['name'] = ''
+                break
+
+            people['id'] = item['people']['id']
+            people['name'] = item['people']['name']
+
+
+def mark_face(frame, peoples):
+    for people in peoples:
+        bb = people['bb']
+        name = "%s#%d" % (people['name'], people['id'])
+
+        scale = 0.2
+
+        image.printFaceBox(frame, scale, bb)
+        image.printName(frame, name, scale, bb)
+
+
+def detect_face(frame):
+    global face_seq
+
+    bbs = faceapi.allFaceBoundingBoxes(frame)
+    peoples = []
+    for bb in bbs:
+        landmarks = faceapi.findLandmarks(frame, bb)
+        face = faceapi.align(frame, bb, landmarks=landmarks)
+
+        if face is None:
+            continue
+
+        people = {}
+        people['seq'] = face_seq
+        people['eigen'] = faceapi.getEigen(face)
+        people['bb'] = bb
+
+        face_seq += 1
+
+    # call predict api here
+    predict_face(peoples)
+    return peoples
+
 
 def main(args):
     try:
@@ -27,9 +106,17 @@ def main(args):
         pipe = subprocess.Popen(ff_param, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 
         while (cap.isOpened()):
-            ret, frame = cap.read()
+            ret, origin = cap.read()
             if ret == True:
-                frame = cv2.resize(frame, (640, 480))
+                peoples = detect_face(origin)
+
+                if len(peoples):
+                    mark_face(origin, peoples)
+
+                width = int(args.size.split('x')[0])
+                height = int(args.size.split('x')[1])
+
+                frame = cv2.resize(origin, (width, height))
 
                 # sleep(0.5)
 
@@ -50,4 +137,4 @@ def main(args):
 
 
 if __name__ == '__main__':
-    main(parser.parse_args())
+    main()
